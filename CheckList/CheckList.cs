7c09e1e30b9943be1019568b1c5e59558ab2c558
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-10-15 20:39:39 +1100

    Add project files.

---
+++ CheckList/CheckList.cs
@@ -0,0 +1,80 @@
+﻿// Parts adapted from https://dotnetcodr.com/2020/10/13/roll-your-own-custom-list-with-c-net/.
+
+using System.Collections;
+
+namespace CheckList
+{
+	public class CheckList<CheckListItem> : IEnumerable<CheckListItem>
+	{
+		protected internal static CheckListItem[] Data;
+
+		/// <summary>
+		/// The number of elements in the list.
+		/// </summary>
+		public int Count { get { return Data.Length; } }
+
+		public CheckList(int capacity = 0)
+		{
+			Data = new CheckListItem[capacity];
+		}
+
+		private void Resize()
+		{
+			CheckListItem[] array = new CheckListItem[Count + 1];
+
+			for (int index = 0; index < Data.Length; index++)
+			{
+				array[index] = Data[index];
+			}
+
+			Data = array;
+		}
+
+		/// <summary>
+		/// Adds an item to the end of the list.
+		/// </summary>
+		/// <param name="item"></param>
+		public void Add(CheckListItem item)
+		{
+			Resize();
+
+			Data[Count - 1] = item;
+		}
+
+		/// <summary>
+		/// Removed the item from the list.
+		/// </summary>
+		/// <param name="item"></param>
+		public void Remove(CheckListItem item)
+		{
+			int index = Array.IndexOf(Data, item);
+			Data = Data.Where<CheckListItem>((value, i) => i != index).ToArray();
+		}
+
+		public IEnumerator<CheckListItem> GetEnumerator()
+		{
+			for (int index = 0; index < Count; index++)
+			{
+				yield return Data[index];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return Data.GetEnumerator();
+		}
+
+		/// <summary>
+		/// Gets an item at the specified index.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public CheckListItem this[int index]
+		{
+			get
+			{
+				return Data[index];
+			}
+		}
+	}
+}
---
+++ CheckList/CheckListItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckList
+{
+	public class CheckListItem
+	{
+		/// <summary>
+		/// The value of this CheckListItem.
+		/// </summary>
+		public dynamic Value { get; set; }
+
+		/// <summary>
+		/// The Checked state of this CheckListItem.
+		/// </summary>
+		public bool Checked { get; set; }
+
+		/// <summary>
+		/// Checks the CheckListItem.
+		/// </summary>
+		public void Check()
+		{
+			Checked = true;
+		}
+
+		/// <summary>
+		/// Unchecks the CkeckListItem.
+		/// </summary>
+		public void Uncheck()
+		{
+			Checked = false;
+		}
+
+		/// <summary>
+		/// Changes this CheckListItem to another.
+		/// </summary>
+		/// <param name="item">The item to change this CheckListItem to.</param>
+		/// <exception cref="NullReferenceException"></exception>
+		public void Change(CheckListItem item)
+		{
+			if (item != null)
+			{
+				this.Value = item.Value;
+				this.Checked = item.Checked;
+			}
+			else
+			{
+				throw new NullReferenceException();
+			}
+		}
+	}
+}
